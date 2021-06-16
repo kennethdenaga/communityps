e44311e7ebd64a2efade9693ee6ecb20e309c77f
kennethdenaga <85311798+kennethdenaga@users.noreply.github.com>
2021-06-16 19:14:09 +0800

    Update urls.py

--- BossKen/urls.py
+++ BossKen/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from kenlist import views 
 
 urlpatterns = [    
-    url(r'^$', views.home_page, name='home_page'),    
+    url(r'^$', views.home_page, name=''),    
     url(r'^kenlist/new$', views.new_donator, name='new_donator'),    
     url(r'^kenlist/(\d+)/$', views.view_donator, name='view_donator'),    
     url(r'^kenlist/(\d+)/add_donation$', views.add_donation, name='add_donation'),]
